c9e1f4a5821016f197c506e21df4256f371a14ec
krishsam94 <krishsam94@gmail.com>
2026-08-14 13:06:24 +0000

    Adding

---
+++ src/main/java/Ex3ReversingArr.java
@@ -0,0 +1,27 @@
+public class Ex3ReversingArr {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Original array:");
+        printArray(arr);
+        reverseArray(arr);
+        System.out.println("Reversed array:");
+        printArray(arr);
+    }
+
+    public static void reverseArray(int[] arr) {
+        // Reverse array by swapping elements from both ends
+        for(int strtIdx = 0; strtIdx < arr.length / 2; strtIdx++) {
+            int temp = arr[strtIdx];
+            int endIndex = arr.length - 1;
+            arr[strtIdx] = arr[endIndex - strtIdx];
+            arr[endIndex - strtIdx] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
