a7f5db898caab17bfa9c3c6e4a3d6f2fcadf44be
krishsam94 <krishsam94@gmail.com>
2026-09-12 15:55:41 +0530

    Terst

---
+++ src/main/java/Ex5BinarySearchAsc.java
@@ -0,0 +1,29 @@
+public class Ex5BinarySearchAsc {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 9, 14, 23, 33, 67, 87, 90};
+        int target = 35;
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end -start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
--- src/main/java/samplejava/Ex3PrimeNumber.java
+++ src/main/java/samplejava/Ex3PrimeNumber.java
@@ -1,11 +1,33 @@
-int num = 12321;
-if (num <= 1) print("Not prime");
-else if (num%2 == 0) print("Not prime");
-else {
-	int itr = 3;
-	while(itr * itr < num) {
-		if (num%itr == 0) print("Not prime");
-		itr += 2;
-	}	
+package samplejava;
+public class Ex3PrimeNumber {
+	public static void main(String[] args) {
+		int num = 113;
+		isPrime(num);
+	}
+
+	private static void isPrime(int num) {
+		if (num <= 1) {
+			System.out.println("Not prime");
+			return;
+		} else if (num == 2) {
+			System.out.println("Prime");
+			return;
+		} else if (num % 2 == 0) {
+			System.out.println("Not prime");
+			return;
+		} else {
+			int itr = 3;
+			// Changed < to <= to correctly handle perfect squares
+			while (itr * itr <= num) {
+				if (num % itr == 0) {
+					System.out.println("Not prime");
+					return;
+				}
+				itr += 2;
+			}
+		}
+
+		// If no divisors were found, it is prime
+		System.out.println("Prime");
+	}
 }
-print("Prime");
