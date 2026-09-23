c42b53211b8121f523ff0abf8dd1464c750248c2
krishsam94 <krishsam94@gmail.com>
2026-09-23 16:50:15 +0530

    Add reference link to LeetCode problem
    Added a link to the related problem for reference.

--- src/main/java/Ex16SearchInRotatedArray.java
+++ src/main/java/Ex16SearchInRotatedArray.java
@@ -1,5 +1,6 @@
 public class Ex16SearchInRotatedArray {
     // https://leetcode.com/problems/search-in-rotated-sorted-array/
+    // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2};
         int target = 0;
